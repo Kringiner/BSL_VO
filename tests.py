afc868ed95b95176e73913e211ecbe9be9ce2231
Xsitin <m9ckom@gmail.com>
2021-11-08 17:04:50 +0500

    tests

--- tests.py
+++ tests.py
@@ -1,2 +1,13 @@
+from vector import Vector
+
+
 def test_passing():
     assert (1, 2, 3) == (1, 2, 3)
+    v1 = Vector(1, 2)
+    v2 = Vector(1, 2)
+    result = v1.add(v2)
+    assert result == Vector(2, 4)
+    result = v1.subtract(v2)
+    assert result == Vector(0, 0)
+    result = v1.scalar_mult(v2)
+    assert result == 5
