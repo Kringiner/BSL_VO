2ab90780b32b0b9ce222173153b3d5f92d7231d2
Kringiner <mr.aloner@mail.ru>
2021-11-08 16:54:04 +0500

    Добавил сигнатуры методов

--- vector.py
+++ vector.py
@@ -2,3 +2,12 @@ class Vector:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+    def add(self, vector):
+        pass
+
+    def subtract(self, vector):
+        pass
+
+    def scalar_mult(self, vector):
+        pass
